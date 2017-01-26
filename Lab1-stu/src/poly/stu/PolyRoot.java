c5e27216123432abb0f899023f155c9f78109e7c
ducphanduyagentp <ducphanduy.agentp@gmail.com>
2017-01-26 15:14:28 -0500

    Add Lab1

--- Lab1-stu/src/poly/stu/PolyRoot.java
+++ Lab1-stu/src/poly/stu/PolyRoot.java
@@ -1,7 +1,19 @@
 package poly.stu;
+import java.util.ArrayList;
 
 /**
  * Created by ptnega on 26/01/2017.
  */
 public class PolyRoot {
+    public static final double EPSILON = 0.0001;
+    public static final double INITIAL_GUESS = 0.1;
+    public static final int MAX_ITERATIONS = 100;
+
+    public static double computeRoot(ArrayList<Integer> poly) {
+
+    }
+
+    private static double newtonsMethod(ArrayList<Integer> poly, double x0, int iter) {
+
+    }
 }
