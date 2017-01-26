9c9f15bd0d1bd0794e06312ead79ebfd56f71ba4
ddp3945 <ddp3945@heartofgold.cs.rit.edu>
2017-01-26 15:49:54 -0500

    Add Lab1

--- Lab1-stu/src/poly/stu/PolyDerive.java
+++ Lab1-stu/src/poly/stu/PolyDerive.java
@@ -8,9 +8,13 @@ public class PolyDerive {
 
     public static ArrayList<Integer> computeDerivative(ArrayList<Integer> poly) {
         ArrayList<Integer> derivatives = new ArrayList<Integer>();
-        for (int i = 0; i < poly.size(); i++ ) {
+        for (int i = 1; i < poly.size(); i++ ) {
             derivatives.add(poly.get(i) * i);
         }
+        if (derivatives.size() == 0) {
+            derivatives.add(0);
+        }
+
         return  derivatives;
     }
 }
